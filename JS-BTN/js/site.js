9e92213e762d76372f8e4ce3b14f45ff3640663f
umutcansimsek75 <119808448+umutcansimsek75@users.noreply.github.com>
2023-02-27 22:36:28 +0300

    JS-BTN

---
+++ JS-BTN/js/site.js
@@ -0,0 +1,9 @@
+function yazim_buyult() {
+    var yazi = document.getElementById("1");
+    yazi.innerHTML = yazi.innerHTML.toUpperCase();
+}
+
+function yazim_kucult() {
+    var yazi = document.getElementById("1");
+    yazi.innerHTML = yazi.innerHTML.toLowerCase();
+}
