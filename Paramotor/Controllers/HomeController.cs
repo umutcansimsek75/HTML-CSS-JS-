366cfa84536accc88e64c56a234f8dcdd074178e
umutcansimsek75 <umutcansimsek75@gmail.com>
2023-05-10 21:31:35 +0300

    Asp.net Paramotor Part-1

---
+++ Paramotor/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using occupy.Models;
+using occupy.Models.Entities;
+
+namespace occupy.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    ParamatordbContext db = new ParamatordbContext();
+
+    public IActionResult Index()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/about-us")]
+    public IActionResult About()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/service")]
+    public IActionResult Service()
+    {
+        return View();
+    }
+
+    [Route("/event")]
+    public IActionResult Event()
+    {
+        return View();
+    }
+
+    [Route("/blog")]
+    public IActionResult Blog()
+    {
+        return View();
+    }
+
+    [Route("/contact")]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
+    }
+}
---
+++ Paramotor/Models/Entities/ParamatordbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace occupy.Models.Entities;
+
+public partial class ParamatordbContext : DbContext
+{
+    public ParamatordbContext()
+    {
+    }
+
+    public ParamatordbContext(DbContextOptions<ParamatordbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Site> Sites { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;port=3306;database=paramatordb;user=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.27-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_turkish_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Site>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("site");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.Email)
+                .HasMaxLength(250)
+                .HasColumnName("email");
+            entity.Property(e => e.Facebook)
+                .HasMaxLength(250)
+                .HasColumnName("facebook");
+            entity.Property(e => e.Favicon)
+                .HasMaxLength(250)
+                .HasColumnName("favicon");
+            entity.Property(e => e.Instagram)
+                .HasMaxLength(250)
+                .HasColumnName("instagram");
+            entity.Property(e => e.Logo)
+                .HasMaxLength(250)
+                .HasColumnName("logo");
+            entity.Property(e => e.Title)
+                .HasMaxLength(250)
+                .HasColumnName("title");
+            entity.Property(e => e.Twitter)
+                .HasMaxLength(250)
+                .HasColumnName("twitter");
+            entity.Property(e => e.Url)
+                .HasMaxLength(250)
+                .HasColumnName("url");
+            entity.Property(e => e.Youtube)
+                .HasMaxLength(250)
+                .HasColumnName("youtube");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
